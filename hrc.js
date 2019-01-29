032b505ca87e090d0cffd77623ad7f452ac2f491
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-01-29 11:16:05 -0600

    on 42

--- hrc.js
+++ hrc.js
@@ -1243,3 +1243,132 @@ function getNthElement(array, n) {
   return array[n];
 };
 ***************************************************************************************************************************************
+preImmersive-buildingBlocksMastery-039-getFirstElement
+
+Write a function called “getFirstElement”.
+
+Given an array, “getFirstElement” returns the first element of the given array.
+
+Notes:
+* If the given array has a length of 0, it should return undefined.
+
+var output = getFirstElement([1, 2, 3, 4, 5]);
+console.log(output); // --> 1
+
+Your Code Should Pass:
+
+describe("getFirstElement", function() {
+  it("should return the first element of an array", function() {
+    expect(getFirstElement([1, 3, 5])).toBe(1);
+  });
+  it("should return undefined if the array is empty", function() {
+    expect(getFirstElement([])).toBe(undefined);
+  });
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function getFirstElement(array) {
+  return array[0];
+};
+**************************************************************************************************************************************
+preImmersive-buildingBlocksMastery-040-getLastElement
+
+Write a function called “getLastElement”.
+
+Given an array, “getLastElement” returns the last element of the given array.
+
+Notes:
+* If the given array has a length of 0, it should return ‘undefined’.
+
+var output = getLastElement([1, 2, 3, 4]);
+console.log(output); // --> 4
+
+Your Code Should Pass:
+
+describe("getLastElement", function() {
+  it("should return the last element of an array", function() {
+    expect(getLastElement([1, 3, 5])).toBe(5);
+  });
+  it("should return undefined if the array is empty", function() {
+    expect(getLastElement([])).toBe(undefined);
+  });
+});
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function getLastElement(array) {
+  return array[array.length-1];
+};  
+***************************************************************************************************************************************
+preImmersive-buildingBlocksMastery-041-addToFront
+
+Write a function called “addToFront”.
+
+Given an array and an element, “addToFront” adds the given element to the front of the given array, and returns the given array.
+
+Notes: 
+* It should be the SAME array, not a new array.
+
+var output = addToFront([1, 2], 3);
+console.log(output); // -> [3, 1, 2]
+
+Your Code Should Pass:
+
+describe("addToFront", function() {
+  it("should return an array", function() {
+    expect(Array.isArray(addToFront([1, 2], 3))).toBe(true);
+  });
+
+  it("addToFront should add an element to the beginning of an array", function() {
+    expect(addToFront([1, 2], 3)).toEqual([3, 1, 2]);
+  });
+
+  it("should add an element to the beginning of an empty array", function() {
+    expect(addToFront([], 3)).toEqual([3]);
+  });
+
+  it("should be the same array instance that was passed in", function() {
+    var input = [1, 2, 3];
+    expect(addToFront(input, 4)).toBe(input);
+  });
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function addToFront(arr, element) {
+  arr.unshift(element);
+  return arr;
+}
+***********************************************************************************************************************************
+preImmersive-buildingBlocksMastery-042-addToBack
+
+Write a function called “addToBack”.
+
+Given an array and an element, “addToBack” returns the given array with the given element added to the end.
+
+Note: It should be the SAME array, not a new array.
+
+var output = addToBack([1, 2], 3);
+console.log(output); // -> [1, 2, 3]
+
+Your Code Should Pass:
+
+describe("addToBack", function() {
+  it("should return an array", function() {
+    expect(Array.isArray(addToBack([1, 2], 3))).toBe(true);
+  });
+
+  it("should add an element to the end of an array", function() {
+    expect(addToBack([1, 2], 3)).toEqual([1, 2, 3]);
+  });
+
+  it("should add an element to the end of an empty array", function() {
+    expect(addToBack([], 3)).toEqual([3]);
+  });
+
+  it("should be the same array instance that was passed in", function() {
+    var input = [1, 2, 3];
+    expect(addToBack(input, 4)).toBe(input);
+  });
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function addToBack(arr, element) {
+  arr.push(element);
+  return arr;
+}
+******************************************************************************************************************************************
