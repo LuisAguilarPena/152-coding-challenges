89fe77d6698770b0abadb84b7272ee175766c428
LuisAguilarPena <46686358+LuisAguilarPena@users.noreply.github.com>
2019-06-19 06:26:18 -0500

    work up to 46

--- 1-50.js
+++ 1-50.js
@@ -308,5 +308,330 @@ function average(numbers) {
 }
 
 /*
-27.
+27. Write a function called “computeAreaOfATriangle”.
+
+Given the base and height of a triangle, “computeAreaOfATriangle” returns its area.
+
+var output = computeAreaOfATriangle(4, 6);
+console.log(output); // --> 12
+*/
+
+const computeAreaOfATriangle = (base, height) => base*height/2;
+
+/*
+28. Write a function called “cube”.
+
+Given a number, “cube” returns the cube of that number.
+
+var output = cube(3);
+console.log(output); // --> 27
+*/
+
+const cube = num => num*num*num;
+
+/*
+29. Write a function called “square”.
+
+Given a number, “square” should return the square of the given number.
+
+var output = square(5);
+console.log(output); // --> 25
+*/
+
+const square = num => num*num;
+
+/*
+30. Write a function called “computeAverageLengthOfWords”.
+
+Given two words, “computeAverageLengthOfWords” returns the average of their lengths.
+
+var output = computeAverageLengthOfWords('code', 'programs');
+console.log(output); // --> 6
+*/
+
+const computeAverageLengthOfWords = (str1, str2) => (str1.length + str2.length )/2;
+
+/*
+31. Write a function called “addFullNameProperty”.
+
+Given an object that has a “firstName” property and a “lastName” property, 
+“addFullNameProperty” returns a “fullName” property whose value is a string with the first name and 
+last name separated by a space.
+
+var person = {
+  firstName: 'Jade',
+  lastName: 'Smith'
+};
+addFullNameProperty(person);
+console.log(person.fullName); // --> 'Jade Smith' 
+*/
+
+const addFullNameProperty = obj => obj.fullName = `${obj.firstName} ${obj.lastName}`; 
+
+/*
+32. Write a function called “addObjectProperty”.
+
+Given two objects and a key, “addObjectProperty” sets a new property on the 1st object at 
+the given key. The value of that new property is the entire 2nd object.
+
+var person1 = {
+  name: 'Joe Blow',
+  role: 'schlub'
+};
+var person2 = {
+  name: 'Mr. Burns',
+  role: 'supervisor'
+};
+addObjectProperty(person1, 'manager', person2);
+console.log(person1.manager); // --> { name: 'Mr. Burns', role: 'supervisor' }
+*/
+
+const addObjectProperty = (objToRecieve, key, objAsValue) => objToRecieve[key] = objAsValue;
+
+/*
+33. Write a function called “isPersonOldEnoughToDrinkAndDrive”.
+
+Given a “person” object, that contains an “age” property, 
+“isPersonOldEnoughToDrinkAndDrive” returns whether the given person is 
+old enough to legally drink and drive in the United States.
+
+Notes:
+* The legal drinking age in the United States is 21.
+* The legal driving age in the United States is 16.
+* It is always illegal to drink and drive in the United States.
+
+var obj = {
+  age: 45
+};
+var output = isPersonOldEnoughToDrinkAndDrive(obj);
+console.log(output); // --> false
+*/
+
+const isPersonOldEnoughToDrinkAndDrive = obj => false;
+
+/*
+34. Write a function called “isPersonOldEnoughToDrive”.
+
+Given a “person” object, that contains an “age” property, 
+“isPersonOldEnoughToDrive” returns whether the given person is old enough to drive.
+
+Notes:
+* The legal driving age in the United States is 16.
+
+var obj = {
+  age: 16
+};
+var output = isPersonOldEnoughToDrive(obj);
+console.log(output); // --> true
+*/
+
+const isPersonOldEnoughToDrive = obj => obj.age>=16 ? true : false; 
+
+/*
+35. Write a function called “isPersonOldEnoughToVote”.
+
+Given a “person” object, that contains an “age” property, 
+“isPersonOldEnoughToVote” returns whether the given person is old enough to vote.
+
+Notes:
+* The legal voting age in the United States is 18.
+
+var obj = {
+  age: 19
+};
+var output = isPersonOldEnoughToVote(obj);
+console.log(output); // --> true
+*/
+
+const isPersonOldEnoughToVote = obj => obj.age>=18 ? true : false; 
+
+/*
+36. Write a function called “isPersonOldEnoughToDrink”.
+
+Given a “person” object, that contains an “age” property, 
+“isPersonOldEnoughToDrink” returns whether the given person is old enough to drink.
+
+Notes:
+* The legal drinking age in the United States is 21.
+
+var obj = {
+  age: 16
+};
+var output = isPersonOldEnoughToDrink(obj);
+console.log(output); // --> false
+*/
+
+const isPersonOldEnoughToDrink = obj => obj.age>=21 ? true : false;
+
+/*
+37. Write a function called “addArrayProperty”.
+
+Given an object, a key, and an array, “addArrayProperty” sets a new property on the object 
+at the given key, with its value set to the given array.
+
+var myObj = {};
+var myArray = [1, 3];
+addArrayProperty(myObj, 'myProperty', myArray);
+console.log(myObj.myProperty); // --> [1, 3]
+*/
+
+const addArrayProperty = (obj, key, arrayAsValue) => obj[key]=arrayAsValue;
+
+/*
+38. Write a function called “getNthElement”.
+
+Given an array and an integer, “getNthElement” returns the element at the given integer,
+within the given array.
+
+Notes:
+* If the array has a length of 0, it should return ‘undefined’.
+
+var output = getNthElement([1, 3, 5], 1);
+console.log(output); // --> 3
+*/
+
+const getNthElement = (arr, integer) => arr[integer];
+
+/*
+39. Write a function called “getFirstElement”.
+
+Given an array, “getFirstElement” returns the first element of the given array.
+
+Notes:
+* If the given array has a length of 0, it should return undefined.
+
+var output = getFirstElement([1, 2, 3, 4, 5]);
+console.log(output); // --> 1
+*/
+
+const getNthElement = arr => arr[0];
+
+/*
+40. Write a function called “getLastElement”.
+
+Given an array, “getLastElement” returns the last element of the given array.
+
+Notes:
+* If the given array has a length of 0, it should return ‘undefined’.
+
+var output = getLastElement([1, 2, 3, 4]);
+console.log(output); // --> 4
+*/
+
+const getLastElement = arr => arr[arr.length-1]; 
+
+/*
+41. Write a function called “addToFront”.
+
+Given an array and an element, “addToFront” adds the given element to the front of the given array, 
+and returns the given array.
+
+Notes: 
+* It should be the SAME array, not a new array.
+
+var output = addToFront([1, 2], 3);
+console.log(output); // -> [3, 1, 2]
+
+*/
+
+const addToFront = (arr, element) => arr.unshift(element);
+
+/*
+42. Write a function called “addToBack”.
+
+Given an array and an element, “addToBack” returns the given array with the given element added to the end.
+
+Note: It should be the SAME array, not a new array.
+
+var output = addToBack([1, 2], 3);
+console.log(output); // -> [1, 2, 3]
+
+*/
+
+const addToBack = (arr, element) => arr.push(element);
+
+/*
+43. Write a function called “computeAreaOfARectangle”.
+
+Given the length and width of a rectangle, “computeAreaOfARectangle” returns its area.
+
+var output = computeAreaOfARectangle(4, 8);
+console.log(output); // --> 32
+
+*/
+
+const computeAreaOfRectangle =(length, width) => length*width;
+
+/*
+44. Write a function called “computePerimeterOfARectangle”.
+
+Given a length and a width describing a rectangle, “computePerimeterOfARectangle” returns its perimter.
+
+var output = computePerimeterOfARectangle(5, 2);
+console.log(output); // --> 14
+
+*/
+
+const computePerimeterOfArectangle = (length, width) => length+length+width+width;
+
+/*
+45. Write a function called “computePerimeterOfATriangle”.
+
+Given 3 sides describing a triangle, “computePerimeterOfATriangle” returns its perimter.
+
+var output = computePerimeterOfATriangle(6, 4, 10);
+console.log(output); // --> 20
+
+*/
+
+const computePerimeterOfATriangle = (a, b, c) => a+b+c;
+
+/*
+46. Write a function called “computeTripledAreaOfARectangle”.
+
+Given a length and width of a rectangle, “computeTripledAreaOfARectangle” 
+returns the rectangle’s area, multiplied by 3.
+
+var output = computeTripledAreaOfARectangle(2, 4);
+console.log(output); // --> 12
+
+*/
+
+const computeTripleAreaOfArectangle = (length, width) => ((length*width)/2)*3;
+
+/*
+47. Write a function called “computePerimeterOfACircle”.
+
+Given the radius of a circle, “computePerimeterOfACircle” returns its perimeter.
+
+var output = computePerimeterOfACircle(4);
+console.log(output); // --> 25.132741228718345
+
+*/
+
+/*
+48. Write a function called “computeAreaOfACircle”.
+
+Given the radius of a circle, “computeAreaOfACircle” returns its area.
+
+var output = computeAreaOfACircle(4);
+console.log(output); // --> 50.26548245743669
+*/
+
+/*
+49. Write a function called “computePower”.
+
+Given a number and an exponent, “computePower” returns the given number, raised to the given exponent.
+
+var output = computePower(2, 3);
+console.log(output); // --> 8
+
+*/
+
+/*
+50. Write a function called “computeSquareRoot”.
+Given a number, “computeSquareRoot” returns its square root.
+
+var output = computeSquareRoot(9);
+console.log(output); // --> 3
 */
